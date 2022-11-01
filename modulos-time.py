a972a4173bbfde9cdf07c096f91c84af10bf1287
LNMIG <migliorenicolas@yahoo.com.ar>
2022-11-01 12:19:44 -0300

    DOCS: ejercicio 2 de Módulos creado
    El archivo se llama modulos-time.py

---
+++ modulos-time.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+date = datetime.now()
+day = datetime.today().strftime('%A')
+hour = date.hour + round(date.minute/60,2) + round(date.second/3600,2)
+
+if day != 'Sunday' or day != 'Saturday':
+    if hour > 19:
+        print("Ya es hora de ir a casa")
+    else:
+        resto = round(abs(hour - 19),2)
+        print("Faltan",resto,"hs para ir a casa")
+else:
+    print("En este día no trabajas")
